c0eca28c9b2894e905b17905555598517e4330ce
jundaeyoung <jdy1787@naver.com>
2023-02-21 15:13:50 +0900

    버블 움직임 추가

---
+++ bubble/src/ex06/exPlayer.java
@@ -0,0 +1,152 @@
+package ex06;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class exPlayer extends JLabel implements Moveable {
+
+	private int x;
+	private int y;
+	private ImageIcon playerR, playerL;
+
+	private boolean left;
+	private boolean right;
+	private boolean up;
+	private boolean down;
+
+	private final int SPEED = 4;
+	private final int JUMPSPEED = 2;
+
+	public void setLeft(boolean left) {
+		this.left = left;
+	}
+
+	public void setRight(boolean right) {
+		this.right = right;
+	}
+
+	public void setUp(boolean up) {
+		this.up = up;
+	}
+
+	public void setDown(boolean down) {
+		this.down = down;
+	}
+
+	public exPlayer() {
+		initData();
+		setInitLayout();
+	}
+
+	public void initData() {
+		playerR = new ImageIcon("images/playerR.png");
+		playerL = new ImageIcon("images/playerL.png");
+
+	}
+
+	public void setInitLayout() {
+		x = 55;
+		y = 535;
+		setSize(50, 50);
+		setLocation(x, y);
+		setIcon(playerR);
+
+	}
+
+	@Override
+	public void left() {
+		left = true;
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (left) {
+					setIcon(playerL);
+					x -= SPEED;
+					setLocation(x, y);
+
+					if (x <= 70) {
+						x += SPEED;
+					}
+					try {
+						Thread.sleep(10);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}).start();
+	}
+
+	@Override
+	public void right() {
+		right = true;
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (right) {
+					setIcon(playerR);
+					x += SPEED;
+					setLocation(x, y);
+
+					if (x > 880) {
+						x -= SPEED;
+					}
+					try {
+						Thread.sleep(10);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}).start();
+	}
+
+	@Override
+	public void up() {
+		up = true;
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				for (int i = 0; i < 59; i++) {
+					if (y <= 0) {
+						y += JUMPSPEED;
+					}
+					y -= JUMPSPEED;
+					setLocation(x, y);
+					try {
+						Thread.sleep(5);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				up = false;
+				down();
+			}
+		}).start();
+
+	}
+
+	@Override
+	public void down() {
+		down = true;
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				for (int i = 0; i < 59; i++) {
+					if (y >= 535) {
+						y -= JUMPSPEED;
+					}
+					y += JUMPSPEED;
+					setLocation(x, y);
+					try {
+						Thread.sleep(3);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		}).start();
+
+	}
+}
